08c411dda9b0d1832503f35c42cddb10cb238552
R. Tyler Croy <tyler@monkeypox.org>
2011-07-16 17:05:08 -0700

    Add a simple module to take care of setting up my user account

---
+++ modules/user-tyler/manifests/init.pp
@@ -0,0 +1,50 @@
+class user-tyler {
+    group {
+        "tyler" :
+            gid     => 5501,
+            ensure  => present;
+    }
+
+    user {
+        "tyler" :
+            uid     => 5500,
+            gid     => 5501,
+            groups  => "infraadmin",
+            shell   => "/usr/bin/zsh",
+            home    => "/home/tyler",
+            ensure  => present,
+            require => [
+                        Group["infraadmin"],
+                        Package["zsh"]
+                        ];
+    }
+
+    file {
+        "/home/tyler" :
+            ensure      => directory,
+            require     => User["tyler"],
+            owner       => "tyler",
+            group       => "tyler";
+        "/home/tyler/.ssh" :
+            ensure      => directory,
+            require     => File["/home/tyler"],
+            owner       => "tyler",
+            group       => "tyler";
+    }
+
+    ssh_authorized_key {
+        "tyler" :
+            user        => "tyler",
+            ensure      => present,
+            require     => File["/home/tyler/.ssh"],
+            key         => "AAAAB3NzaC1yc2EAAAABIwAAAQEAueiy12T5bvFhsc9YjfLc3aVIxgySd3gDxQWy/bletIoZL8omKmzocBYJ7F58U1asoyfWsy2ToTOY8jJp1eToXmbD6L5+xvHba0A7djYh9aQRrFam7doKQ0zp0ZSUF6+R1v0OM4nnWqK4n2ECIYd+Bdzrp+xA5+XlW3ZSNzlnW2BeWznzmgRMcp6wI+zQ9GMHWviR1cxpml5Z6wrxTZ0aX91btvnNPqoOGva976B6e6403FOEkkIFTk6CC1TFKwc/VjbqxYBg4kU0JhiTP+iEZibcQrYjWdYUgAotYbFVe5/DneHMLNsMPdeihba4PUwt62rXyNegenuCRmCntLcaFQ==",
+            type        => "rsa",
+            name        => "tyler@kiwi";
+    }
+
+
+    package {
+        "zsh" :
+            ensure => installed;
+    }
+}
