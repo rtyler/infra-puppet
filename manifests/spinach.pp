b54c2e272a7ab48eec8f144c505ccf3c0908dd58
R. Tyler Croy <tyler@monkeypox.org>
2011-07-17 18:36:49 -0700

    Add a root manifest for the spinach build slave

---
+++ manifests/spinach.pp
@@ -0,0 +1,15 @@
+#
+#   Root manifest to be run on spinach (Rackspace-donated build machine)
+#
+
+# Not explicitly specifying a regex to match to spinach.jenkins-ci.org since
+# the machine is configured with a hostname of "jenkins-01" for some goofy
+# reason
+
+node default {
+    include users-core
+
+    include ntpdate
+
+    include ci-ssh-slave
+}
