e68c1623370da1960a1caf37ce75ca6a814db349
R. Tyler Croy <tyler@monkeypox.org>
2011-08-25 16:02:52 -0700

    Update the apache2 dependency to pull from OBS on CentOS
    The EPEL apache2 package is just too old for MirrorBrain

--- modules/pkg-apache2/manifests/init.pp
+++ modules/pkg-apache2/manifests/init.pp
@@ -1,17 +1,19 @@
 
 class pkg-apache2 {
     if $operatingsystem == "CentOS" {
-        package {
-            "httpd" :
-                alias   => apache2,
-                ensure  => installed;
+        yumrepo {
+            # We need at least Apache 2.2.6 for MirrorBrain
+            "Apache" :
+                baseurl => "http://download.opensuse.org/repositories/Apache/CentOS_5/",
+                descr => "Apache OBS repo",
+                enabled => 1,
+                gpgcheck => 0;
         }
+
+        Yumrepo["Apache"] -> Package["apache2"]
     }
-    else {
-        package {
-            "apache2" :
-                alias   => apache2,
-                ensure  => installed;
-        }
+    package {
+        "apache2" :
+            ensure  => installed;
     }
 }
