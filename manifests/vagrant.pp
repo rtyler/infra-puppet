88d00a1746f70399fd400def23870a803d973e8d
R. Tyler Croy <tyler@monkeypox.org>
2011-08-25 22:57:30 -0700

    Avoid running these other manifests I don't need for testing

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -1,14 +1,11 @@
 node default {
     include users-core
-    include user-kbsingh
 
-    include ntpdate
-
-    include nagios-server
-
-    include haproxy-main
-
-    include ci-ssh-slave
+    #include user-kbsingh
+    #include ntpdate
+    #include nagios-server
+    #include ci-ssh-slave
+    #include haproxy-main
 
     include mirrorbrain
 }
