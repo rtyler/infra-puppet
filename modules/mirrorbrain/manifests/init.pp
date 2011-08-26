79c632c74a0f6a3b591117214af1ea5ed04cf5b1
R. Tyler Croy <tyler@monkeypox.org>
2011-08-25 22:57:05 -0700

    Work in progress, getting files in place for mod_mirrorbrain, which we won't install from OBS

--- modules/mirrorbrain/manifests/init.pp
+++ modules/mirrorbrain/manifests/init.pp
@@ -15,6 +15,7 @@ class mirrorbrain {
 }
 
 class mirrorbrain::centos {
+    include mirrorbrain::cron
     include mirrorbrain::files
     include mirrorbrain::tree
     include mirrorbrain::repos
@@ -23,7 +24,8 @@ class mirrorbrain::centos {
     Class["mirrorbrain::repos"] ->
         Class["mirrorbrain::packages"] ->
             Class["mirrorbrain::tree"] ->
-                Class["mirrorbrain::files"]
+                Class["mirrorbrain::files"] ->
+                    Class["mirrorbrain::cron"]
 }
 
 
@@ -39,21 +41,21 @@ class mirrorbrain::repos {
 
 class mirrorbrain::packages {
     package {
-        "apache2-mod_geoip" :
+        "mod_geoip" :
             ensure => installed,
             require => Package["apache2"];
-        "apache2-mod_asn" :
-            ensure => installed,
-            require => Package["apache2"];
-        "apache2-mod_form" :
-            ensure => installed,
-            require => Package["apache2"];
-        "apache2-mod_mirrorbrain" :
-            ensure => installed,
-            require => [
-                        Package["apache2"],
-                        Package["apache2-mod_geoip"]
-                       ];
+        #"apache2-mod_asn" :
+        #    ensure => installed,
+        #    require => Package["apache2"];
+        #"apache2-mod_form" :
+        #    ensure => installed,
+        #    require => Package["apache2"];
+        #"apache2-mod_mirrorbrain" :
+        #    ensure => installed,
+        #    require => [
+        #                Package["apache2"],
+        #                Package["apache2-mod_geoip"]
+        #               ];
 
         "postgresql-devel" :
             ensure => installed;
@@ -71,38 +73,72 @@ class mirrorbrain::packages {
 
 class mirrorbrain::tree {
     file {
-        "/etc/apache2" :
+        "/etc/httpd" :
             ensure  => directory,
             require => Class["pkg-apache2"];
 
-        "/etc/apache2/vhosts.d" :
+        "/etc/httpd/conf.d" :
             ensure  => directory,
             require => [
                         Class["pkg-apache2"],
-                        File["/etc/apache2"]
-                       ];
-
-        "/etc/sysconfig/apache2" :
-            ensure => directory,
-            require => [
-                        Class["pkg-apache2"]
+                        File["/etc/httpd"]
                        ];
     }
 }
 
 class mirrorbrain::files {
     file {
-        "/etc/apache2/vhosts.d/mirrors.conf" :
+        "/etc/httpd/conf.d/mirrors.conf" :
             ensure  => present,
             require => Class["pkg-apache2"],
             source  => "puppet:///modules/mirrorbrain/virtualhost.conf";
 
-        "/etc/sysconfig/apache2/geoip.conf" :
+        "/etc/httpd/conf.d/geoip.conf" :
             ensure  => present,
             require => [
-                        Class["pkg-apache2"],
-                        File["/etc/sysconfig/apache2"]
+                        Class["pkg-apache2"]
                        ],
             source  => "puppet:///modules/mirrorbrain/geoip.conf";
+
+
+        "/etc/mirmon.conf" :
+            ensure  => present,
+            source  => "puppet:///modules/mirrorbrain/mirmon.conf";
+
+        "/etc/mirrorbrain.conf" :
+            ensure  => present,
+            source  => "puppet:///modules/mirrorbrain/mirrorbrain.conf";
+    }
+}
+
+class mirrorbrain::cron {
+    cron {
+        "mirrorprobe" :
+            command => "mirrorprobe",
+            user    => root,
+            minute  => 30,
+            ensure  => present;
+
+        "mb scan" :
+            command => "mb scan --quiet --jobs 4 --al",
+            user    => root,
+            minute  => 45,
+            ensure  => present;
+
+        "mb vacuum" :
+            command => "mb db vacuum",
+            user    => root,
+            minute  => 30,
+            hour    => 1,
+            weekday => Monday,
+            ensure  => present;
+
+        #"geoip-lite-update" :
+        #    command => "geoip-lite-update",
+        #    user    => root,
+        #    minute  => 45,
+        #    hour    => 4,
+        #    weekday => Tuesday,
+        #    ensure  => present;
     }
 }
--- modules/pkg-apache2/manifests/init.pp
+++ modules/pkg-apache2/manifests/init.pp
@@ -1,19 +1,29 @@
 
 class pkg-apache2 {
     if $operatingsystem == "CentOS" {
-        yumrepo {
-            # We need at least Apache 2.2.6 for MirrorBrain
-            "Apache" :
-                baseurl => "http://download.opensuse.org/repositories/Apache/CentOS_5/",
-                descr => "Apache OBS repo",
-                enabled => 1,
-                gpgcheck => 0;
-        }
+        #yumrepo {
+        #    # We need at least Apache 2.2.6 for MirrorBrain
+        #    "Apache" :
+        #        baseurl => "http://download.opensuse.org/repositories/Apache/CentOS_5/",
+        #        descr => "Apache OBS repo",
+        #        enabled => 1,
+        #        gpgcheck => 0;
+        #}
 
-        Yumrepo["Apache"] -> Package["apache2"]
+        package {
+            "httpd" :
+                alias => "apache2",
+                ensure => installed;
+
+            "httpd-devel" :
+                ensure => installed;
+        }
     }
-    package {
-        "apache2" :
-            ensure  => installed;
+    else {
+        package {
+            "apache2" :
+                alias => "apache2",
+                ensure  => installed;
+        }
     }
 }
