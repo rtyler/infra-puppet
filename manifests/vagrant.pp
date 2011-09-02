4b406e70af11031743d2047f84f75fe4fa2833cc
R. Tyler Croy <tyler@monkeypox.org>
2011-09-01 19:04:29 -0700

    Update the vagrant manifest to define a standard sudo_role

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -1,11 +1,13 @@
 node default {
+    $sudo_role = "standard"
+
     include users-core
+    include sudo
 
     #include user-kbsingh
     #include ntpdate
     #include nagios-server
     #include ci-ssh-slave
     #include haproxy-main
-
-    include mirrorbrain
+    #include mirrorbrain
 }
