0ab32244684cd8670cff55b3c810e1002acb6404
R. Tyler Croy <tyler@monkeypox.org>
2011-09-01 19:03:58 -0700

    Add a basic sudo module

---
+++ modules/sudo/manifests/init.pp
@@ -0,0 +1,16 @@
+
+class sudo {
+
+    package {
+        sudo:
+            ensure => latest,
+    }
+
+    file { "/etc/sudoers":
+        owner   => root,
+        group   => root,
+        mode    => 440,
+        source  => "puppet:///modules/sudo/$sudo_role-sudoers",
+        require => Package["sudo"],
+    }
+}
