849f6028cc3d787549234bd4ee140310434b80d1
R. Tyler Croy <tyler@monkeypox.org>
2011-09-05 18:23:10 -0700

    Dump the output from run.sh to a file in $CWD

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 HOSTNAME=`hostname -s`
+LOGFILE="puppet.`date "+%s"`.log"
 
 echo "==> Preparing to run ${HOSTNAME}.pp, Ctrl-C immediately to abort"
 sleep 5
+
+echo
+echo "==> Generating log file: ${LOGFILE}"
+
+exec &> $LOGFILE
+
 
 git pull --rebase && puppet apply --modulepath=modules --verbose manifests/${HOSTNAME}.pp
