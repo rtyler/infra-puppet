39b1bc7969a5e048cbfbfeb64e80c93499c24d33
R. Tyler Croy <tyler@monkeypox.org>
2011-09-17 17:12:57 -0700

    Avoid installing apache2 packages on non-Ubuntu hosts

--- modules/pkg-apache2/manifests/init.pp
+++ modules/pkg-apache2/manifests/init.pp
@@ -1,23 +1,7 @@
 
 class pkg-apache2 {
-    if $operatingsystem == "CentOS" {
-        #yumrepo {
-        #    # We need at least Apache 2.2.6 for MirrorBrain
-        #    "Apache" :
-        #        baseurl => "http://download.opensuse.org/repositories/Apache/CentOS_5/",
-        #        descr => "Apache OBS repo",
-        #        enabled => 1,
-        #        gpgcheck => 0;
-        #}
-
-        package {
-            "httpd" :
-                alias => "apache2",
-                ensure => installed;
-
-            "httpd-devel" :
-                ensure => installed;
-        }
+    if $operatingsystem != "Ubuntu" {
+        err("pkg-apache2 is not configured for $operatingsystem")
     }
     else {
         package {
