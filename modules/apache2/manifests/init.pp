690e2c342faa332ef71e95bbf7acf85935bf967a
R. Tyler Croy <tyler@monkeypox.org>
2011-09-17 18:47:41 -0700

    Add the custom task enable-apache-mod for enabling apache2 modules and reloading apache2

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -11,3 +11,40 @@ class apache2 {
         }
     }
 }
+
+class apache2::functions {
+    exec {
+        "reload-apache2" :
+            refreshonly => true,
+            command => "/etc/init.d/apache2 reload",
+            require => Package["apache2"];
+
+        "restart-apache2" :
+            refreshonly => true,
+            command => "/etc/init.d/apache2 restart",
+            require => Package["apache2"];
+    }
+}
+
+define enable-apache-mod($mod_name) {
+    include apache2::functions
+
+    $availible_file_path = "/etc/apache2/mods-available/${mod_name}.load"
+    $enabled_file_path = "/etc/apache2/mods-enabled/${mod_name}.load"
+
+    file {
+        $availible_file_path :
+            ensure => present,
+    }
+
+    exec {
+        "enable-${mod_name}" :
+            require => [
+                           Package["apache2"],
+                           File[$availible_file_path],
+                       ],
+            unless => "test -f ${enabled_file_path}",
+            command => "a2enmod ${mod_name}",
+            notify => Exec["reload-apache2"];
+    }
+}
