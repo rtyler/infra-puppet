2ac7cd8c9f4e8d18d674436dd0840b5240e94070
Jesse Farinacci <jieryn@gmail.com>
2011-09-22 21:26:01 -0400

    adding user-jieryn

---
+++ modules/user-jieryn/manifests/init.pp
@@ -0,0 +1,44 @@
+class user-jieryn {
+    group {
+        "jieryn" :
+            gid     => 5508,
+            ensure  => present;
+    }
+
+    user {
+        "jieryn" :
+            uid     => 5507,
+            gid     => 5508,
+            groups  => "infraadmin",
+            shell   => "/bin/bash",
+            home    => "/home/jieryn",
+            ensure  => present,
+            require => [
+                        Group["jieryn"],
+                        Group["infraadmin"],
+                        ];
+    }
+
+    file {
+        "/home/jieryn" :
+            ensure      => directory,
+            require     => User["jieryn"],
+            owner       => "jieryn",
+            group       => "jieryn";
+        "/home/jieryn/.ssh" :
+            ensure      => directory,
+            require     => File["/home/jieryn"],
+            owner       => "jieryn",
+            group       => "jieryn";
+    }
+
+    ssh_authorized_key {
+        "jieryn" :
+            user        => "jieryn",
+            ensure      => present,
+            require     => File["/home/jieryn/.ssh"],
+            key         => "AAAAB3NzaC1yc2EAAAADAQABAAABAQC/aWXsTuSt6b0BrjhIwZQjzE/n3PYlbWYOCYI35aLWJxoYxiLjdvSLmmiv9tMjy5w7q8FEWmTXYtIrrKbjb2Twbkj/iRcGTzQYrd0JoyrX4MNBhXxjYlBZepdoGnbVm1rsrPDI1kVgAfBGxe9LLgTv81NSaTyyTw8gW2rpEMAwjbm3iBWHscsHAI8smQJnO6F3Oy07MAt5notu/LElmtV8m2eKHYp4WyZxarEW+rLtYivHk18QDxc9cEuY6ThZpBM3cWze/AFWEb7KOCG8vGVDEZmU8I5Q8b+dcI/1LeRLwc6GcTrVgCjQoay8bW17Hf7csn2pETHV2orxyq+Yv6WD",
+            type        => "rsa",
+            name        => "jesse@ayn";
+    }
+}
