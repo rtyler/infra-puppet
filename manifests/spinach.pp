e4a6523eda6a1ce2c810e7b5e168747b8c86bd36
R. Tyler Croy <tyler@monkeypox.org>
2011-11-01 16:21:41 -0700

    Give spinach a global Exec path too (stupid exec crap)

--- manifests/spinach.pp
+++ manifests/spinach.pp
@@ -2,12 +2,12 @@
 #   Root manifest to be run on spinach (Rackspace-donated build machine)
 #
 
-# Not explicitly specifying a regex to match to spinach.jenkins-ci.org since
-# the machine is configured with a hostname of "jenkins-01" for some goofy
-# reason
-
-node default {
+node /^spinach$/ {
     include base
     include user-jieryn
     include ci-ssh-slave
+}
+
+Exec {
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]
 }
