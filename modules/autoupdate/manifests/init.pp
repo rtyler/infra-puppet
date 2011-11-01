ffe3d5f48ced6e99fd22fd694db433dd213c732c
R. Tyler Croy <tyler@monkeypox.org>
2011-11-01 16:19:55 -0700

    Avoid creating the autoupdate git repo unless it's not already there

--- modules/autoupdate/manifests/init.pp
+++ modules/autoupdate/manifests/init.pp
@@ -35,5 +35,8 @@ class autoupdate::setup {
             creates => "/root/infra-puppet",
             command => "git clone git://github.com/jenkinsci/infra-puppet.git",
             require => Package["git-core"],
+            # In the case of a new machine, we probably already have this
+            unless  => "test -d /root/infra-puppet/.git",
+            path    => ["/usr/bin", "/usr/local/bin"],
     }
 }
