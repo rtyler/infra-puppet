34dd40b753fd5a6765b05d985e769eeb33b9dc73
R. Tyler Croy <tyler@monkeypox.org>
2011-12-04 13:55:01 -0800

    Turns out some versions of puppet don't like my silly whitespace in the SSH key

--- modules/nagios-client/manifests/init.pp
+++ modules/nagios-client/manifests/init.pp
@@ -35,7 +35,7 @@ class nagios-client {
                 user    => "nagios",
                 ensure  => "present",
                 require => File["/var/lib/nagios/.ssh"],
-                key     => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAs2ST7fNG1/UDkt8YhvgMa8DFiHPooyjQ3vxDE0Pt1HRJN4qwAS5mNTlEGvJnAJS243BTqhzmIE/UeC76vEvWzJazQt9fmPpjKYhsm0BQfG/oxkOcLWRNxrfw9GgwEiVw68qqJl6vfCmlwJseIiJQJX5kbC99HURsG0+7Vo3eyQt2BP7kgFQrH7o5hvF+ca0MPt2YVPn4Btm20PpZ5o8BR/KjFkzsORSs52wvzp4pJMDpEnIyCa2g4HQ57ucDQtluuIaV01lrzaMohhg2ZORIoUrHX2mBbR2Id1PCA84f9EE4Rqia3G2cdYM62ewqgxUQyTqXhgWDQ1X51DEJgvU1jw==",
+                key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAs2ST7fNG1/UDkt8YhvgMa8DFiHPooyjQ3vxDE0Pt1HRJN4qwAS5mNTlEGvJnAJS243BTqhzmIE/UeC76vEvWzJazQt9fmPpjKYhsm0BQfG/oxkOcLWRNxrfw9GgwEiVw68qqJl6vfCmlwJseIiJQJX5kbC99HURsG0+7Vo3eyQt2BP7kgFQrH7o5hvF+ca0MPt2YVPn4Btm20PpZ5o8BR/KjFkzsORSs52wvzp4pJMDpEnIyCa2g4HQ57ucDQtluuIaV01lrzaMohhg2ZORIoUrHX2mBbR2Id1PCA84f9EE4Rqia3G2cdYM62ewqgxUQyTqXhgWDQ1X51DEJgvU1jw==",
                 type    => "rsa",
                 name    => "nagios@${hostname}";
     }
