5f56c689e417f3f57de63eb4f5c7b7bdb7048ba2
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-13 14:26:25 -0800

    defined disk space monitoring

--- modules/nagios-server/manifests/checks.pp
+++ modules/nagios-server/manifests/checks.pp
@@ -20,6 +20,23 @@ class nagios-server::checks {
             # Runs: wiki, issues
             name => "eggplant";
     }
+
+    # Define how Nagios monitors remote disk via SSH login
+    $nagios_config_root = "/etc/nagios3/conf.d/jenkins"
+    nagios_command {
+        "check_disk_by_ssh":
+            command_line    => "$USER1$/check_by_ssh -H $HOSTADDRESS$ -C \"/usr/lib/nagios/plugins/check_disk -w $ARG1$ -c $ARG2$\"",
+            notify          => Service["nagios"],
+            target          => "${nagios_config_root}/check_disk_by_ssh.cfg",
+            ensure          => present
+    }
+
+    nagios-check-disk {
+        "eggplant" :
+            name => "eggplant";
+        "cucumber" :
+            name => "cucumber";
+    }
 }
 
 define nagios-check-http ($name) {
@@ -53,4 +70,22 @@ define nagios-check-https ($name) {
             use                 => "generic-service",
     }
 }
+
+# set up the disk monitoring service for the given host
+define nagios-check-disk ($name) {
+    $nagios_config_root = "/etc/nagios3/conf.d/jenkins"
+
+    nagios_service {
+        "disk check $name" :
+            target              => "${nagios_config_root}/disk.cfg",
+            notify              => Service["nagios"],
+            ensure              => present,
+            contact_groups      => "core-admins",
+            service_description => "Disk availability",
+            check_command       => "check_disk_by_ssh!2000!1000",  # warning at 2GB, critical at 1GB. (otherwise use 10%, etc)
+            host_name           => "${name}.jenkins-ci.org",
+            use                 => "generic-service",
+    }
+}
+
 # vim: shiftwidth=4 expandtab tabstop=4
