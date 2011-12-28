44891c5d5fe77afefb460661bc126e0995b3e195
R. Tyler Croy <tyler@monkeypox.org>
2011-12-27 17:47:50 -0800

    Tweak spacing, being a pedant YAY

--- modules/temp-dns/manifests/init.pp
+++ modules/temp-dns/manifests/init.pp
@@ -1,42 +1,42 @@
 
 
 class temp-dns {
-
-  package {
+    package {
     'bind' :
-      ensure => present,
-      name   => 'bind9';
+        ensure => present,
+        name   => 'bind9';
 
     'bind-utils' :
-      ensure => present,
-      name   => 'dnsutils';
+        ensure => present,
+        name   => 'dnsutils';
   }
 
-  file {
-    "/etc/bind/jenkins-ci.org.zone" :
-      ensure  => present,
-      owner   => bind,
-      group   => bind,
-      require => Package["bind"],
-      notify  => Service["bind"],
-      source  => "puppet:///modules/temp-dns/jenkins-ci.org.zone";
+    file {
+        "/etc/bind/jenkins-ci.org.zone" :
+            ensure  => present,
+            owner   => bind,
+            group   => bind,
+            require => Package["bind"],
+            notify  => Service["bind"],
+            source  => "puppet:///modules/temp-dns/jenkins-ci.org.zone";
 
-    "/etc/bind/named.conf.local" :
-      ensure  => present,
-      owner   => bind,
-      group   => bind,
-      notify  => Service["bind"],
-      require => [
-                  File["/etc/bind/jenkins-ci.org.zone"],
-                  Package["bind"]
-      ],
-      source => "puppet:///modules/temp-dns/named.conf.local";
-  }
+        "/etc/bind/named.conf.local" :
+            ensure  => present,
+            owner   => bind,
+            group   => bind,
+            notify  => Service["bind"],
+            require => [
+                        File["/etc/bind/jenkins-ci.org.zone"],
+                        Package["bind"]
+            ],
+            source => "puppet:///modules/temp-dns/named.conf.local";
+    }
 
-  service {
-    "bind" :
-      ensure  => running,
-      require => Package['bind'],
-      name    => 'bind9';
-  }
+    service {
+        "bind" :
+            ensure  => running,
+            require => Package['bind'],
+            name    => 'bind9';
+    }
 }
+# vim: shiftwidth=4 expandtab tabstop=4
