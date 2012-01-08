fef7812fb9d0b3c7639510844540c5171adad046
R. Tyler Croy <tyler@monkeypox.org>
2012-01-07 17:30:01 -0800

    Touch a file every time a puppet run fails, in theory
    I'll come up with how Nagios checks this later. The nagios check should only
    waiver between OK and CRITICAL depending on the presence of the file.

--- run.sh
+++ run.sh
@@ -2,9 +2,12 @@
 
 HOSTNAME=`hostname -s`
 LOGFILE="puppet.`date "+%s"`.log"
+ERROR_FILE="last_run_failed"
 
 echo "==> Preparing to run ${HOSTNAME}.pp, Ctrl-C immediately to abort"
 sleep 5
+
+rm -f ${ERROR_FILE}
 
 echo
 echo "==> Generating log file: ${LOGFILE}"
@@ -12,3 +15,7 @@ echo "==> Generating log file: ${LOGFILE}"
 git pull --rebase >> ${LOGFILE} 2>&1 && \
  git submodule update --init >> ${LOGFILE} 2>&1 && \
  puppet apply --modulepath=modules --verbose manifests/${HOSTNAME}.pp >> ${LOGFILE} 2>&1
+
+if [ $? -ne 0 ]; then
+    touch ${ERROR_FILE}
+fi
