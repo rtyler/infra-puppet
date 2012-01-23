918e391aa0765a377ae275daf05338b9217ee173
R. Tyler Croy <tyler@monkeypox.org>
2012-01-22 21:12:06 -0800

    Let the check_mirrorbrain script return some useful output into nagios

--- modules/nagios/files/check_mirrorbrain.sh
+++ modules/nagios/files/check_mirrorbrain.sh
@@ -4,4 +4,4 @@ CHECK_URL="http://mirrors.jenkins-ci.org/updates/update-center.json"
 
 wget -O /dev/null \
   --debug \
-  "${CHECK_URL}" 2>&1 | grep "X-MirrorBrain" -c
+  "${CHECK_URL}" 2>&1 | grep "X-MirrorBrain"
