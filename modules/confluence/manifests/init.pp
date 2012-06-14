26cb2daa6792860370a6a279505453baead4e650
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-06-13 18:48:52 -0700

    Added firewall rules to allow inbound 80 and 443

--- modules/confluence/manifests/init.pp
+++ modules/confluence/manifests/init.pp
@@ -71,4 +71,17 @@ class confluence {
     }
 
     # TODO: stage https key files
+
+    firewall {
+    '100 accept inbound HTTP requests' :
+        proto  => 'tcp',
+        port   => 80,
+        action => 'accept';
+
+    '101 accept inbound HTTPs requests' :
+        proto  => 'tcp',
+        port   => 443,
+        action => 'accept';
+    }
+
 }
