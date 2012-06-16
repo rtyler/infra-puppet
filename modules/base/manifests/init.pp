834353232a36165d81de2acf70029af59fffa8ec
R. Tyler Croy <tyler@monkeypox.org>
2012-06-16 11:08:07 -0700

    Add the fakeroot package to the base module so any machine can build debs

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -6,129 +6,132 @@
 #   not be loaded on every machine, then it should go elsewhere
 
 class base {
-    include autoupdate
-    include jenkins-dns
-    include sshd
-    include sudo
-    include stdlib
-    include users-core
+  include autoupdate
+  include jenkins-dns
+  include sshd
+  include sudo
+  include stdlib
+  include users-core
 
-    # Unfortunately this module only supports Ubuntu
-    if ($operatingsystem == 'Ubuntu') {
-        include nagios::client
-    }
-
-
-    stage {
-        'pre' :
-            before => Stage['main'];
-        'post' :
-            require => Stage['main'];
-    }
-
-    class {
-        'base::pre' :
-            stage => 'pre';
-
-        'base::post' :
-            stage => 'post';
-
-        'packages::git' :  ;
-        'packages::wget':  ;
-        'packages::ruby':  ;
-        'packages::hiera': ;
-
-        'ntp' :
-            ensure     => running,
-            servers    => ['pool.ntp.org iburst'],
-            autoupdate => true;
-    }
-
-    file {
-        '/etc/puppet' :
-            ensure => directory,
-            owner  => root,
-            group  => root;
-
-        '/etc/puppet/hiera' :
-            ensure  => directory,
-            owner   => root,
-            group   => root,
-            require => [
-                        File['/etc/puppet'],
-                        Class['packages::hiera'],
-            ];
-    }
+  # Unfortunately this module only supports Ubuntu
+  if ($operatingsystem == 'Ubuntu') {
+    include nagios::client
+  }
 
 
-    package {
-        # htop(1) is generally handy, and I like having it around :)
-        'htop' :
-            ensure => present;
-    }
+  stage {
+    'pre' :
+      before => Stage['main'];
+    'post' :
+      require => Stage['main'];
+  }
 
-    group {
-        'puppet' :
-            ensure => present,
-    }
+  class {
+    'base::pre' :
+      stage => 'pre';
 
-    sshd::config {
-        'PermitRootLogin' :
-            value => 'no';
+    'base::post' :
+      stage => 'post';
 
-        'PasswordAuthentication' :
-            value => 'no';
+    'packages::git' :  ;
+    'packages::wget':  ;
+    'packages::ruby':  ;
+    'packages::hiera': ;
 
-        'UseDNS' :
-            value => 'no';
-    }
+    'ntp' :
+      ensure   => running,
+      servers  => ['pool.ntp.org iburst'],
+      autoupdate => true;
+  }
 
-    firewall {
-        '000 accept all icmp requests' :
-            proto  => 'icmp',
-            action => 'accept';
+  file {
+    '/etc/puppet' :
+      ensure => directory,
+      owner  => root,
+      group  => root;
 
-        '001 accept inbound ssh requests' :
-            proto  => 'tcp',
-            port   => 22,
-            action => 'accept';
+    '/etc/puppet/hiera' :
+      ensure  => directory,
+      owner   => root,
+      group   => root,
+      require => [
+            File['/etc/puppet'],
+            Class['packages::hiera'],
+      ];
+  }
 
-        '002 accept local traffic' :
-            # traffic within localhost is OK
-            iniface => 'lo',
-            action => 'accept';
 
-        '003 allow established connections':
-            # this is needed to make outbound connections work, such as database connection
-            state => ['RELATED','ESTABLISHED'],
-            action => 'accept';
+  package {
+    # htop(1) is generally handy, and I like having it around :)
+    'htop' :
+      ensure => present;
+    # fakeroot is handy for building .deb packages
+    'fakeroot' :
+      ensure => present;
+  }
 
-    }
+  group {
+    'puppet' :
+      ensure => present,
+  }
+
+  sshd::config {
+    'PermitRootLogin' :
+      value => 'no';
+
+    'PasswordAuthentication' :
+      value => 'no';
+
+    'UseDNS' :
+      value => 'no';
+  }
+
+  firewall {
+    '000 accept all icmp requests' :
+      proto  => 'icmp',
+      action => 'accept';
+
+    '001 accept inbound ssh requests' :
+      proto  => 'tcp',
+      port   => 22,
+      action => 'accept';
+
+    '002 accept local traffic' :
+      # traffic within localhost is OK
+      iniface => 'lo',
+      action => 'accept';
+
+    '003 allow established connections':
+      # this is needed to make outbound connections work, such as database connection
+      state => ['RELATED','ESTABLISHED'],
+      action => 'accept';
+
+  }
 }
 
 class base::pre {
-    # It's generally useful to make sure our package meta-data is always up to
-    # date prior to running just about everything else
-    if ($operatingsystem == 'Ubuntu') {
-        $command = 'apt-get update'
-    }
-    elsif ($operatingsystem =~ /(RedHat|CentOS)/) {
-        $command = 'yum makecache'
-    }
-    else {
-        err('Unsupported platform!')
-    }
+  # It's generally useful to make sure our package meta-data is always up to
+  # date prior to running just about everything else
+  if ($operatingsystem == 'Ubuntu') {
+    $command = 'apt-get update'
+  }
+  elsif ($operatingsystem =~ /(RedHat|CentOS)/) {
+    $command = 'yum makecache'
+  }
+  else {
+    err('Unsupported platform!')
+  }
 
-    exec {
-        'pre-update packages' :
-            command => $command;
-    }
+  exec {
+    'pre-update packages' :
+      command => $command;
+  }
 }
 
 class base::post {
-    firewall {
-        '999 drop all other requests':
-            action => 'drop';
-    }
+  firewall {
+    '999 drop all other requests':
+      action => 'drop';
+  }
 }
 # vim: shiftwidth=4 expandtab tabstop=4
