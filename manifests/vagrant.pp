1db17fdfe1ca687f869f9d5557edd5d761ba1378
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-08-07 12:03:35 -0700

    testing groovy

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -9,12 +9,7 @@ node /^lucid32$/ {
     ##      - add 'wiki2.jenkins-ci.org' to /etc/hosts as 127.0.0.1
     # include confluence
 
-    include gitrepo
-
-    gitrepo::repo {
-      'all' :
-        description => 'Hi';
-    }
+    include groovy
 
     firewall {
       '100 accept inbound HTTP requests' :
