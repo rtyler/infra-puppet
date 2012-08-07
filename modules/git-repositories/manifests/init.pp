f3b3ff6ed61fbf7ed01bec7d57d907194a5e0fbf
R. Tyler Croy <tyler@monkeypox.org>
2012-08-06 23:06:19 -0700

    Clean up the resource graph for the git-repositories module to make apache2 requirements explicit
    In some cases, such as the placement of the VirtualHost configuration, we need
    apache2 to be fully installed before we try to install the file, otherwise it
    will fail because `/etc/apache` doesn't exist yet

--- modules/git-repositories/manifests/init.pp
+++ modules/git-repositories/manifests/init.pp
@@ -2,57 +2,59 @@
 # Hosts read-only Git repositories and expose it via Apache
 #
 class git-repositories {
-    include apache2
-    include apache2::log-rotation
+  include apache2
+  include apache2::log-rotation
 
-    $gitrepo_dir="/var/www/git.jenkins-ci.org"
+  $gitrepo_dir="/var/www/git.jenkins-ci.org"
 
-    file {
-        $gitrepo_dir :
-            ensure      => directory,
-            owner       => 'www-data',
-            group       => 'www-data'
-    }
+  file {
+    $gitrepo_dir :
+      ensure  => directory,
+      owner   => 'www-data',
+      group   => 'www-data',
+      require => Class['apache2'];
+  }
 
-    git::repository {
-        "all" :
-            description => "Hi"
-    }
+  git::repository {
+    "all" :
+      description => "Hi";
+  }
 
-    package {
-        "gitweb" :
-            ensure      => installed
-    }
+  package {
+    "gitweb" :
+      ensure    => installed;
+  }
 
-    file {
-        "/etc/gitweb.conf":
-            source => 'puppet:///modules/git-repositories/gitweb.conf';
-    }
+  file {
+    "/etc/gitweb.conf":
+      source => 'puppet:///modules/git-repositories/gitweb.conf';
+  }
 
-    apache2::virtualhost {
-        'git.jenkins-ci.org' :
-            source => 'puppet:///modules/git-repositories/git.jenkins-ci.org';
-    }
+  apache2::virtualhost {
+    'git.jenkins-ci.org' :
+        require => Class['apache2'],
+        source    => 'puppet:///modules/git-repositories/git.jenkins-ci.org';
+  }
 }
 
 # create one Git repository
 define git::repository($description) {
-    $gitrepo_dir=$git-repositories::gitrepo_dir
+  $gitrepo_dir=$git-repositories::gitrepo_dir
 
-    exec {
-        "create repository ${name}" :
-            require => File[$gitrepo_dir],
-            unless  => "test -d ${gitrepo_dir}/${name}.git",
-            path    => ['/bin', '/usr/bin'],
-            user    => 'www-data',
-            command => "git init --bare ${gitrepo_dir}/${name}.git";
-    }
+  exec {
+    "create repository ${name}" :
+      require => File[$gitrepo_dir],
+      unless  => "test -d ${gitrepo_dir}/${name}.git",
+      path  => ['/bin', '/usr/bin'],
+      user  => 'www-data',
+      command => "git init --bare ${gitrepo_dir}/${name}.git";
+  }
 
-    file {
-        "${gitrepo_dir}/${name}.git/description":
-            require => Exec["create repository ${name}"],
-            content => $description
-    }
+  file {
+    "${gitrepo_dir}/${name}.git/description":
+      require => Exec["create repository ${name}"],
+      content => $description;
+  }
 }
 
-# vim: shiftwidth=4 expandtab tabstop=4
+# vim: shiftwidth=2 expandtab tabstop=2
