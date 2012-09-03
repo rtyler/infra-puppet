95f249713a2f1f8d15485a5bce7b332836a4a8bd
R. Tyler Croy <tyler@monkeypox.org>
2012-09-03 12:30:47 -0700

    Implement the steps to make the `Only periodically run "apt-get update"` scenario fail

--- features/step_definitions/package_steps.rb
+++ features/step_definitions/package_steps.rb
@@ -1,8 +1,17 @@
 When /^I provision the host twice$/ do
-  pending # express the regexp above with the code you wish you had
+  # /var/lib/apt/lists/partial looks to be a good way to check when apt-get
+  # update was last run
+  step %{I provision the host}
+  vm.ssh_into("stat /var/lib/apt/lists/partial | md5sum > /tmp/partial_stat_1")
+
+  # Run the provision code again
+  vm.bootstrap
+
+  vm.ssh_into("stat /var/lib/apt/lists/partial | md5sum > /tmp/partial_stat_2")
 end
 
 Then /^apt\-get should only run once$/ do
-  pending # express the regexp above with the code you wish you had
+  same = vm.ssh_into('diff -q /tmp/partial_stat_1 /tmp/partial_stat_2')
+  expect(same).to be(true)
 end
 
