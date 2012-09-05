8c12e266558d4aa00cffd22f491a2a26f062b240
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-09-05 15:39:29 -0700

    Added disk cleanup commands to control the disk usage.
    (This is basically script-izing what I just did to put out the nagio
    warning about disk consumption on eggplant.)

--- manifests/eggplant.pp
+++ manifests/eggplant.pp
@@ -16,6 +16,32 @@ node default {
         port   => 443,
         action => 'accept';
     }
+
+    cron {
+      "compress old JIRA logs" :
+        # compress log files that JIRA creates on the disk
+        # TODO: ideally move this to the JIRA module
+        command => 'find /srv/jira/current/logs -mtime +30 -and -type f -print | grep -v \'.gz$\' | xargs gzip',
+        hour    => 1,
+        minute  => 0,
+        weekday => 'Monday';
+
+      "delete old JIRA temp files" :
+        # JIRA JVM creates temporary files that it doesn't always clean up on its own
+        command => 'find /srv/jira/current/temp/ -mtime +30 -print  | xargs rm',
+        hour    => 0,
+        minute  => 30;
+
+      "compress old Confluence logs" :
+        # compress log files that Confluence creates on the disk
+        # TODO: ideally move this to the confluence module
+        command => 'find /srv/wiki/current/logs -mtime +30 -and -type f -print | grep -v \'.gz$\' | xargs gzip',
+        hour    => 2,
+        minute  => 0,
+        weekday => 'Monday';
+
+      # /srv/wiki/current/temp doesn't seem to have any interesting stuff in it
+    }
 }
 Exec {
     path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
