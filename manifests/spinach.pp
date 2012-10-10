c9ea6a2586fb3ce3bff8704c03ea961ebcb6e8f5
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-10-10 11:12:30 -0700

    moving the all repo to spinach since cabbage is dead

--- manifests/spinach.pp
+++ manifests/spinach.pp
@@ -6,6 +6,25 @@ node default {
     include base
     include user-jieryn
     include ci-ssh-slave
+
+  include gitrepo
+
+  gitrepo::repo {
+    'all' :
+      description => 'One repository that contains all Jenkins CI github repositories. "git clone http://git.jenkins-ci.org/all.git" to clone this repository.';
+  }
+
+  firewall {
+    '100 accept inbound HTTP requests' :
+    proto  => 'tcp',
+    port   => 80,
+    action => 'accept';
+
+    '101 accept inbound HTTPs requests' :
+    proto  => 'tcp',
+    port   => 443,
+    action => 'accept';
+  }
 }
 
 Exec {
