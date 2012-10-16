603816d7cd919e68f4342d780ca2d9a469db5b58
R. Tyler Croy <tyler@monkeypox.org>
2012-10-16 00:09:40 -0700

    Turns out the gem command will try to prompt the user without a tty for a confirmation if multiple versions exist.
    Cute.

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -147,7 +147,7 @@ class base::post {
   exec {
     'kill puppet gems' :
       onlyif  => 'gem list | grep puppet',
-      command => 'gem uninstall -x puppet facter',
+      command => 'gem uninstall -ax puppet facter',
       path    => ['/var/lib/gems/1.8/bin', '/usr/local/bin', '/usr/bin', '/bin'];
   }
 }
