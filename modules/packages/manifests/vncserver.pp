407e4833f10b73ee6d0797542eda40629370ad42
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-10-26 18:31:42 -0700

    made to work on RedHat

--- modules/packages/manifests/vncserver.pp
+++ modules/packages/manifests/vncserver.pp
@@ -1,8 +1,15 @@
 
 class packages::vncserver {
-  package {
-    # on Ubuntu
-    'tightvncserver' :
-      ensure => present;
-  }
+    $tightvncserver = $operatingsystem ? {
+        /(RedHat|CentOS)/   => "tightvnc-server",
+        Ubuntu              => "tightvncserver",
+        default             => "tightvncserver",
+    }
+
+    package {
+        # on Ubuntu
+        '$tightvncserver' :
+            ensure => present,
+            alias => "tightvncserver";
+    }
 }
