7858b334aebae0952906528e9de076eec3ec868a
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-02-12 09:34:51 -0800

    the same key by the different name

--- modules/user-kohsuke/manifests/init.pp
+++ modules/user-kohsuke/manifests/init.pp
@@ -31,12 +31,17 @@ class user-kohsuke {
     }
 
     ssh_authorized_key {
-        "kohsuke_0" :
+        "kohsuke_unicorn" :
             user        => "kohsuke",
             ensure      => absent,
-            key         => "AAAAB3NzaC1yc2EAAAABIwAAAQEArSave9EBJ2rP3Hm5PFyiOpfGsPhJwjqdyaVEwQruM0Fa8nWstla7cdSTSs/ClHn7I1uUzQvX+/+6m/HTVy/WIr0cIIxLDm8hXVLfCLddtvxnXx47fJY3ongasYJ4TarIGkMMX/Vg1JpP7XIkMczUSNRyeHg/bGfV+YCPFuSW+cj2M5yMOE1KyIVQQL/JZu7lu80Ara5+RWSITObdiHRpnNzvBdIyhkSCrG0N7QStIBnEaLU//K2AB5GbK/65+k7sklutcH18wSGridQCNJm4ODUxov+vVr2OH3oiv7gyHEE9TypRI9vS0HUmsD+moPq3O8y0xyP8xaJWkz2LKe8/5Q==",
             type        => "rsa",
             name        => "kohsuke@unicorn.2010";
+
+        "kohsuke_unicore" :
+            user        => "kohsuke",
+            ensure      => absent,
+            type        => "rsa",
+            name        => "kohsuke@unicore.2010";
 
         "kohsuke_1" :
             user        => "kohsuke",
