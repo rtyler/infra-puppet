a55496bb19d53d9adebd3e5457030a02874e04dd
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-04-01 15:05:24 -0700

    this needs to defer to the default version from Atlassian

--- modules/jira/files/setenv.sh
+++ modules/jira/files/setenv.sh
@@ -1,4 +1,7 @@
 # sourced by catalina.sh upon start
 
+# read the one shipped by Atlassian first
+. /srv/jira/current/bin/setenv.sh
+
 # insert our override directory to patch up files that's under JIRA
 export CLASSPATH=/srv/jira/base/classes
