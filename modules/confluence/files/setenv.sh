6fb4a8d2a70e94b76fc021832a065b8c9bb5cf0f
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-05-01 11:53:39 -0700

    Trimming a bit off from memory allocation from Wiki.
    Background:
    We've seen OOM situation on eggplant today, and immediately after I adjusted oom_adj, I saw JIRA got killed by OOM killer. So I think we are still eating too much memory.
    
    Looking at top, I see that Confluence is using more memory than JIRA, and now thanks to the static cache, the load on Confluence should be lower. So I'm taking away some memory from Confluence in the hope that this improves the situation
    
      PID USER      PR  NI  VIRT  RES  SHR S %CPU %MEM    TIME+  COMMAND
      886 wiki      20   0 1537m 979m 5120 S   10 39.0  30:59.76 java
    14836 jira      20   0 1163m 859m  18m S    2 34.2   6:05.49 java

--- modules/confluence/files/setenv.sh
+++ modules/confluence/files/setenv.sh
@@ -3,4 +3,4 @@
 # read the one shipped by Atlassian first
 . /srv/wiki/current/bin/setenv.sh
 
-export JAVA_OPTS="-Xms768m -Xmx768m -XX:MaxPermSize=256m $JAVA_OPTS -Djava.awt.headless=true "
+export JAVA_OPTS="-Xms512m -Xmx640m -XX:MaxPermSize=192m $JAVA_OPTS -Djava.awt.headless=true "
