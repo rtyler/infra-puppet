5b6de032e4ed5a7d0c072bf2554494b6225ca098
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-05-01 10:39:01 -0700

    Maverick repository no longer exists on apt.puppetlabs.com
    This causes puppet application to fail.
    
    The proper fix is to bump up from Maverick (10.10) to next LTS (12.04),
    but until that happens I'm making this failure non-fatal

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -125,7 +125,8 @@ class base::pre {
   # It's generally useful to make sure our package meta-data is always up to
   # date prior to running just about everything else
   if ($operatingsystem == 'Ubuntu') {
-    $command = 'apt-get update && touch /tmp/repos-updated'
+    # hack until we upgrade to supported versions of Ubuntu
+    $command = 'apt-get update ; touch /tmp/repos-updated'
   }
   elsif ($operatingsystem =~ /(RedHat|CentOS)/) {
     $command = 'yum makecache && touch /tmp/repos-updated'
