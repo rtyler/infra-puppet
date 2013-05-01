94fc15cc30f6ccceb95bb415445f7ff49bb1ac2d
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-05-01 11:49:24 -0700

    put the CATALINA_BASE under puppet.
    This will help us better externalize the configuration without touching Confluence too much

---
+++ modules/confluence/files/setenv.sh
@@ -0,0 +1,4 @@
+# sourced by catalina.sh upon start
+
+# read the one shipped by Atlassian first
+. /srv/wiki/current/bin/setenv.sh
--- modules/confluence/manifests/init.pp
+++ modules/confluence/manifests/init.pp
@@ -3,9 +3,35 @@
 #
 class confluence {
   file {
+  [
+    "/srv/wiki/base",
+    "/srv/wiki/base/bin",
+    "/srv/wiki/base/classes",
+    "/srv/wiki/base/conf",
+    "/srv/wiki/base/logs",
+    "/srv/wiki/base/webapps",
+    "/srv/wiki/base/work",
+    "/srv/wiki/base/temp"
+  ]:
+    ensure => "directory",
+    owner => "wiki",
+    group => "wiki",
+    mode => 755
+    ;
+  "/srv/wiki/base/bin/setenv.sh":
+    source => "puppet:///modules/wiki/setenv.sh"
+    ;
+  "/srv/wiki/base/conf/server.xml":
+    source => "puppet:///modules/wiki/server.xml"
+    ;
+  "/srv/wiki/base/conf/web.xml":
+    ensure => "link",
+    target => "../../current/conf/web.xml"
+    ;
   "/etc/init.d/confluence":
     # TODO: run "update-rc.d confluence defaults" when this file change
     source => "puppet:///modules/confluence/confluence.init",
     mode => 755
+    ;
   }
 }
