75af78b7126ce6b56e1a9c83cf63477e3e4aa765
R. Tyler Croy <tyler@monkeypox.org>
2013-07-20 13:49:30 -0700

    Add ruby-devel package to kale so it can build librarian-puppet properly
    Really, it's the json gem's native extensions that require ruby-devel

--- manifests/kale.pp
+++ manifests/kale.pp
@@ -7,6 +7,18 @@ node default {
     include base
 
     include ci-ssh-slave
+
+    package {
+      # Needed to build librarian-puppet, which is technically needed to
+      # bootstrap the host, but we'll keep this dependency here to express that
+      # this is a dependency
+      'ruby-devel' : 
+        require  => Package[gcc],
+        ensure => present;
+
+      'gcc' :
+        ensure => present;
+    }
 }
 
 Exec {
