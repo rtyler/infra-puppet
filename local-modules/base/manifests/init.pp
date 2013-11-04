3c7233d98d4f9e04f00a4513c8d1cccd37314d0e
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-11-03 21:58:43 -0800

    This expression would match more than just "puppet".
    Example:
    
    root@eggplant:~/infra-puppet# gem list | grep puppet
    librarian-puppet (0.9.9)
    root@eggplant:~/infra-puppet# gem uninstall -ax puppet facter
    ERROR:  While executing gem ... (Gem::InstallError)
        cannot uninstall, check `gem list -d puppet`

--- local-modules/base/manifests/init.pp
+++ local-modules/base/manifests/init.pp
@@ -149,7 +149,7 @@ class base::post {
 
   exec {
     'kill puppet gems' :
-      onlyif  => 'gem list | grep puppet',
+      onlyif  => 'gem list | grep ^puppet',
       command => 'gem uninstall -ax puppet facter',
       path    => ['/var/lib/gems/1.8/bin', '/usr/local/bin', '/usr/bin', '/bin'];
   }
